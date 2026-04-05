8cd293094e6e366afa7d2ecc40ef66719210a180
Link4Electronics <link4electronics@gmail.com>
2026-04-05 05:07:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,32 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q optiimage | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.kde.optiimage.svg
+export DESKTOP=/usr/share/applications/org.kde.optiimage.desktop
+export STARTUPWMCLASS=org.kde.optiimage
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/optiimage \
+/usr/bin/anim_diff \
+/usr/bin/anim_dump \
+/usr/bin/cwebp     \
+/usr/bin/dwebp     \
+/usr/bin/gif2webp  \
+/usr/bin/img2webp  \
+/usr/bin/jpegoptim \
+/usr/bin/oxipng    \
+/usr/bin/scour     \
+/usr/bin/webpinfo  \
+/usr/bin/webpmux
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
